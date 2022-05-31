7c79acd8197aabdcbfebd710be536e85d6b560b8
chaudhuree <chaudhuree00@gmail.com>
2022-05-31 19:19:21 +0600

    docs:initialization of name

--- staticMehtod.py
+++ staticMehtod.py
@@ -1,12 +1,13 @@
 class StaticMethod:
-    def name(self):
-        self.name="chaudhuree"
+    def __init__(self,name):
+        self.name=name
     @staticmethod
     def add(a,b):
         return a+b
     # in static method no need to add self
 
 
-stat = StaticMethod()
+stat = StaticMethod('chaudhuree')
+print(stat.name)
 print(stat.add(1,2))
 print(StaticMethod.add(3,4))
